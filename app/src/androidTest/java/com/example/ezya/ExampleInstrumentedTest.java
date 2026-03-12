471282f6743b7bb4ef507b7caad4d4edeaaa288f
Vafelny Toster <genjavinogradov@gmail.com>
2026-03-12 10:15:32 +0300

    Test2

--- app/src/androidTest/java/com/example/ezya/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ezya/ExampleInstrumentedTest.java
@@ -21,6 +21,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
+        //Test
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.ezya", appContext.getPackageName());
     }
